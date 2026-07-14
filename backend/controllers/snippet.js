91b57805ab41a898536797a4c12424a7e3fafbd7
Pranjal Jain <pranjaljain19a@gmail.com>
2026-07-14 18:15:01 +0530

    Add snippet filtering API

--- backend/controllers/snippet.js
+++ backend/controllers/snippet.js
@@ -1,10 +1,31 @@
 const Snippet = require("../models/snippet");
 
 exports.getSnippet = (req, res, next) => {
-  return res.json({
-    success: true,
-    message: "Snippet fetched successfully",
-  });
+  const DummyUserId = "68750b2cf55e1d0e1d7a1234";
+  const { language, type} = req.query;
+  const page = Number(req.query.page) || 1;
+  const limit = 10;
+  const skip = (page - 1) * limit;
+  const query = {userId: DummyUserId};
+  if(language) query.language = language;
+  if(type) query.type = type;
+  
+  Snippet.find(query)
+    .sort({ createdAt: -1 })
+    .skip(skip)
+    .limit(limit)
+    .then((snippets) => {
+      return res.status(200).json({
+        success: true,
+        data: snippets,
+      });
+    })
+    .catch((err) => {
+      return res.status(500).json({
+        success: false,
+        message: err.message,
+      });
+    });
 };
 
 exports.postSnippet = (req, res, next) => {
@@ -32,7 +53,7 @@ exports.postSnippet = (req, res, next) => {
     errorMessage,
     cause,
     fixCode,
-    isPublic
+    isPublic,
   });
   snippet
     .save()
--- backend/routes/snippet.js
+++ backend/routes/snippet.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const snippetRouter = express.Router();
+const { getSnippet } = require('../controllers/snippet');
 const { postSnippet } = require('../controllers/snippet'); 
 
+snippetRouter.get('/snippets', getSnippet);
 snippetRouter.post('/snippets',postSnippet);
 
 exports.snippetRouter = snippetRouter;
